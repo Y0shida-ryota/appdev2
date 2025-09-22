9b724597d8d245f3040b6999a8a757d442d83f39
Fuzimoto Syouhei <B0024029@ib.yic.ac.jp>
2025-09-22 14:45:11 +0900

    名簿を作ている

---
+++ シフト表　野中 隆斗/シフト表　野中 隆斗/Staff_List/CreateStaff_list.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace シフト表_野中_隆斗.Staff_List
+{
+    internal class CreateStaff_list
+    {
+        private DataGridView Staff_List;
+
+        public CreateStaff_list(DataGridView Staff_List)
+        {
+            this.Staff_List = Staff_List;
+        }
+        public void InitializeGrid()
+        {
+            Staff_List.Columns.Clear();//いったん全部消す
+            Staff_List.AutoGenerateColumns = false;//DataGridbiewの自動追加をなしにする
+
+            Staff_List.Columns.Add(new DataGridViewTextBoxColumn()
+            {
+                Name = "StaffID",
+                HeaderText = "ID",
+                ReadOnly = true,
+
+            });
+            Staff_List.Columns.Add(new DataGridViewTextBoxColumn()
+            {
+                Name = "StaffName",
+                HeaderText = "名前",
+                ReadOnly = true,
+            });
+            Staff_List.Columns.Add(new DataGridViewTextBoxColumn()
+            {
+                Name = "Tell",
+                HeaderText = "電話番号",
+                ReadOnly = true,
+            });
+            Staff_List.Columns.Add(new DataGridViewTextBoxColumn()
+            {
+                Name = "e_mail",
+                HeaderText = "メールアドレス",
+                ReadOnly = true,
+            });
+            Staff_List.Columns.Add(new DataGridViewTextBoxColumn()
+            {
+                Name = "bank_acconunt",
+                HeaderText = "銀行口座",
+                ReadOnly = true,
+            });
+            Staff_List.Columns.Add(new DataGridViewTextBoxColumn()
+            {
+                Name = "address",
+                HeaderText = "住所",
+                ReadOnly = true,
+            });
+            Staff_List.Columns.Add(new DataGridViewTextBoxColumn()
+            {
+                Name = "how",
+                HeaderText = "通勤手段",
+                ReadOnly = true,
+            });
+        }
+    }
+}
---
+++ シフト表　野中 隆斗/シフト表　野中 隆斗/Staff_List/InStaff_List.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace シフト表_野中_隆斗.Staff_List
+{
+    internal class InStaff_List
+    {
+        DataTable StaffDate;
+        DataGridView Staff_List;
+        public InStaff_List(DataGridView Staff_List, DataTable StaffDate)
+        {
+            this.StaffDate = StaffDate;
+            this.Staff_List = Staff_List;
+        }
+        public void InStaff()
+        {
+            Staff_List.Rows.Clear(); // 既存行をクリア
+            foreach (DataRow row in StaffDate.Rows)
+            {
+                int rowIndex = Staff_List.Rows.Add();//新しい行を作ってインデックスを追加
+                Staff_List.Rows[rowIndex].Cells["StaffID"].Value = row["staff_id"];
+                Staff_List.Rows[rowIndex].Cells["StaffName"].Value = row["staff_name"];
+                Staff_List.Rows[rowIndex].Cells["Tell"].Value = row["tell"];
+                Staff_List.Rows[rowIndex].Cells["e_mail"].Value = row["e_mail"];
+                Staff_List.Rows[rowIndex].Cells["bank_acconunt"].Value = row["bank_acconunt"];
+                Staff_List.Rows[rowIndex].Cells["address"].Value = row["address"];
+                Staff_List.Rows[rowIndex].Cells["how"].Value = row["how"];
+            }
+        }
+    }
+}
