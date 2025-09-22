a267a468bdf483a9161c45cda9af3524626195a3
Yoshida Ryota <B0024034@ib.yic.ac.jp>
2025-09-22 14:47:50 +0900

    名簿の追加ができるようになった

---
+++ シフト表　野中 隆斗/シフト表　野中 隆斗/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace シフト表_野中_隆斗
+{
+    internal class DBHelper
+    {
+        public DBHelper()
+        {
+        }
+        public static string connectionstring
+        {
+            get
+            {
+                return "Server=PB-B0024029\\SORIMACHI2022;Database=nonaka;User ID=sa;Password=shoo0127;TrustServerCertificate=True;";
+            }
+        }
+
+
+    }
+}
