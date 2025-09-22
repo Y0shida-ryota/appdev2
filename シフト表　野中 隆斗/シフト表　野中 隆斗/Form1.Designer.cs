9aac104c1f858ec4685d2aeb52b5bc6ad03fb518
kentakashiwa <B0024006@ib.yic.ac.jp>
2025-09-22 11:47:41 +0900

    シフト表途中

---
+++ シフト表　野中 隆斗/シフト表　野中 隆斗/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace シフト表_野中_隆斗
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            ID_txt = new TextBox();
+            PS_txt = new TextBox();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(250, 261);
+            button1.Name = "button1";
+            button1.Size = new Size(272, 109);
+            button1.TabIndex = 0;
+            button1.Text = "ログイン";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(163, 98);
+            label1.Name = "label1";
+            label1.Size = new Size(107, 25);
+            label1.TabIndex = 1;
+            label1.Text = "管理者ID ：";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(173, 170);
+            label2.Name = "label2";
+            label2.Size = new Size(97, 25);
+            label2.TabIndex = 2;
+            label2.Text = "パスワード：";
+            // 
+            // ID_txt
+            // 
+            ID_txt.Location = new Point(288, 95);
+            ID_txt.Name = "ID_txt";
+            ID_txt.Size = new Size(234, 31);
+            ID_txt.TabIndex = 3;
+            // 
+            // PS_txt
+            // 
+            PS_txt.Location = new Point(288, 170);
+            PS_txt.Name = "PS_txt";
+            PS_txt.Size = new Size(234, 31);
+            PS_txt.TabIndex = 4;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(356, 37);
+            label3.Name = "label3";
+            label3.Size = new Size(0, 25);
+            label3.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label3);
+            Controls.Add(PS_txt);
+            Controls.Add(ID_txt);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "管理者ログインページ";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+        private Label label2;
+        private TextBox ID_txt;
+        private TextBox PS_txt;
+        private Label label3;
+    }
+}
---
+++ シフト表　野中 隆斗/シフト表　野中 隆斗/Form3.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace シフト表_野中_隆斗
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            monthCalendar2 = new MonthCalendar();
+            button3 = new Button();
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 44);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 57);
+            button1.TabIndex = 0;
+            button1.Text = "マイページ";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(12, 117);
+            button2.Name = "button2";
+            button2.Size = new Size(112, 57);
+            button2.TabIndex = 1;
+            button2.Text = "名簿";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // monthCalendar2
+            // 
+            monthCalendar2.Location = new Point(174, 72);
+            monthCalendar2.Name = "monthCalendar2";
+            monthCalendar2.TabIndex = 3;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(174, 352);
+            button3.Name = "button3";
+            button3.Size = new Size(298, 104);
+            button3.TabIndex = 4;
+            button3.Text = "保存";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(531, 70);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(726, 255);
+            textBox1.TabIndex = 5;
+            // 
+            // Form3
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1299, 501);
+            Controls.Add(textBox1);
+            Controls.Add(button3);
+            Controls.Add(monthCalendar2);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form3";
+            Text = "シフト設定";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private MonthCalendar monthCalendar2;
+        private Button button3;
+        private TextBox textBox1;
+    }
+}
