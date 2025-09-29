78f8cd167166da195ff979a9dd1a29e3aba446dc
kentakashiwa <B0024006@ib.yic.ac.jp>
2025-09-29 10:47:04 +0900

    勤務状況画面が表示されるときIDと名前、勤務開始時間が表示されるようにした。

--- シフト表　野中 隆斗/シフト表　野中 隆斗/Form_working_status.cs
+++ シフト表　野中 隆斗/シフト表　野中 隆斗/Form_working_status.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,13 +16,64 @@ namespace シフト表_野中_隆斗
         public Form_working_status()
         {
             InitializeComponent();
+            LoadShiftData(DateTime.Today);
         }
+
+        private string GetConnectionString()
+        {
+            return "Server=PB-B0024029\\SORIMACHI2022;Database=nonaka;User ID=sa;Password=shoo0127;TrustServerCertificate=True;";
+        }
+
 
         private void returnbtn_Click(object sender, EventArgs e)
         {
             Form2 form2 = new Form2();
             form2.Show();
             this.Hide();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToString("HH:mm:ss");
+        }
+
+        private void LoadShiftData(DateTime selectedDate)
+        {
+            string query = @"
+                SELECT 
+                    s.staff_id AS ID, 
+                    s.staff_name AS 名前, 
+                    sh.start_time AS 勤務開始時間
+                FROM 
+                    shift sh
+                INNER JOIN 
+                    staff s ON sh.staff_id = s.staff_id
+                WHERE 
+                    sh.shift_date = @date
+                ORDER BY 
+                    sh.start_time;
+";
+
+
+            using (SqlConnection conn = new SqlConnection(GetConnectionString()))
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            {
+                cmd.Parameters.AddWithValue("@date", selectedDate.Date); // 日付のみ
+
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+
+                dataGridView1.DataSource = dt;
+
+                
+                if (dataGridView1.Columns.Count >= 3)
+                {
+                    dataGridView1.Columns[0].Width = 80;  
+                    dataGridView1.Columns[1].Width = 150;
+                    dataGridView1.Columns[2].Width = 150; 
+                }
+            }
         }
     }
 }
