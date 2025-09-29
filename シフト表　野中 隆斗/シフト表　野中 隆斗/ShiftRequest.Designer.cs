d27ab37aabab8b887485a7fb695c13022f7bdc02
Fuzimoto Syouhei <B0024029@ib.yic.ac.jp>
2025-09-29 13:52:24 +0900

    ｓ

--- シフト表　野中 隆斗/シフト表　野中 隆斗/ShiftRequest.Designer.cs
+++ シフト表　野中 隆斗/シフト表　野中 隆斗/ShiftRequest.Designer.cs
@@ -37,6 +37,15 @@
             SMinTime = new ComboBox();
             OHTime = new ComboBox();
             OMinTime = new ComboBox();
+            AddButton = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            EndButton = new Button();
             SuspendLayout();
             // 
             // namelabel
@@ -44,9 +53,9 @@
             namelabel.AutoSize = true;
             namelabel.Location = new Point(541, 92);
             namelabel.Name = "namelabel";
-            namelabel.Size = new Size(56, 25);
+            namelabel.Size = new Size(84, 25);
             namelabel.TabIndex = 0;
-            namelabel.Text = "dddd";
+            namelabel.Text = "鳥取砂丘";
             // 
             // IDBox
             // 
@@ -59,7 +68,7 @@
             // yearComboBox
             // 
             yearComboBox.FormattingEnabled = true;
-            yearComboBox.Location = new Point(144, 162);
+            yearComboBox.Location = new Point(192, 162);
             yearComboBox.Name = "yearComboBox";
             yearComboBox.Size = new Size(166, 33);
             yearComboBox.TabIndex = 2;
@@ -83,7 +92,7 @@
             // SHTime
             // 
             SHTime.FormattingEnabled = true;
-            SHTime.Location = new Point(132, 270);
+            SHTime.Location = new Point(192, 270);
             SHTime.Name = "SHTime";
             SHTime.Size = new Size(75, 33);
             SHTime.TabIndex = 5;
@@ -107,16 +116,108 @@
             // OMinTime
             // 
             OMinTime.FormattingEnabled = true;
-            OMinTime.Location = new Point(735, 270);
+            OMinTime.Location = new Point(644, 270);
             OMinTime.Name = "OMinTime";
             OMinTime.Size = new Size(75, 33);
             OMinTime.TabIndex = 8;
+            // 
+            // AddButton
+            // 
+            AddButton.Location = new Point(208, 409);
+            AddButton.Name = "AddButton";
+            AddButton.Size = new Size(190, 95);
+            AddButton.TabIndex = 9;
+            AddButton.Text = "完了";
+            AddButton.UseVisualStyleBackColor = true;
+            AddButton.Click += AddButton_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(225, 92);
+            label1.Name = "label1";
+            label1.Size = new Size(102, 25);
+            label1.TabIndex = 10;
+            label1.Text = "依頼先　ID";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(72, 165);
+            label2.Name = "label2";
+            label2.Size = new Size(102, 25);
+            label2.TabIndex = 11;
+            label2.Text = "依頼勤務日";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(368, 170);
+            label3.Name = "label3";
+            label3.Size = new Size(30, 25);
+            label3.TabIndex = 12;
+            label3.Text = "年";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(66, 273);
+            label4.Name = "label4";
+            label4.Size = new Size(120, 25);
+            label4.TabIndex = 13;
+            label4.Text = "依頼勤務時間";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(273, 273);
+            label5.Name = "label5";
+            label5.Size = new Size(30, 25);
+            label5.TabIndex = 14;
+            label5.Text = "：";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(603, 273);
+            label6.Name = "label6";
+            label6.Size = new Size(30, 25);
+            label6.TabIndex = 15;
+            label6.Text = "：";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(431, 273);
+            label7.Name = "label7";
+            label7.Size = new Size(30, 25);
+            label7.TabIndex = 16;
+            label7.Text = "～";
+            // 
+            // EndButton
+            // 
+            EndButton.Location = new Point(522, 409);
+            EndButton.Name = "EndButton";
+            EndButton.Size = new Size(190, 95);
+            EndButton.TabIndex = 17;
+            EndButton.Text = "キャンセル";
+            EndButton.UseVisualStyleBackColor = true;
+            EndButton.Click += EndButton_Click;
             // 
             // ShiftRequest
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1002, 582);
+            Controls.Add(EndButton);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(AddButton);
             Controls.Add(OMinTime);
             Controls.Add(OHTime);
             Controls.Add(SMinTime);
@@ -144,5 +245,14 @@
         private ComboBox SMinTime;
         private ComboBox OHTime;
         private ComboBox OMinTime;
+        private Button AddButton;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Button EndButton;
     }
 }
